d333c0a091c4ece97382fc83e002e254e9facea3
DavidMBurke <burkedm94@gmail.com>
2022-08-19 15:28:04 -0400

    commiting so Warren can see

---
+++ client/components/NeuralNet.js
@@ -0,0 +1,35 @@
+// import data from './dataLocation';
+import ml5 from 'ml5';
+import dummyData from './DummyData';
+
+
+const options = {
+    task: 'classification',
+    debug: true
+}
+
+const nn = ml5.neuralNetwork(options);
+
+dummyData.forEach(item => {
+    let copy = item;
+    const output = {
+        drawing: copy.drawing
+    }
+    delete copy.drawing;
+    //console.log("input:", copy, "output", output)
+    nn.addData(copy, output);
+})
+
+nn.normalizeData();
+
+const trainingOptions = {
+    epochs: 32,
+    batchSize: 12,
+}
+nn.train(trainingOptions, finishedTraining);
+
+function finishedTraining() {
+    console.log("finished training!")
+}
+
+export default nn;
