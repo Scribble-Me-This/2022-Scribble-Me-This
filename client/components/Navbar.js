1be487d948fa2479882a1a467c451da8e54483a7
AlexKazz <akazenoff@gmail.com>
2022-08-24 12:53:00 -0700

    add logout functionality

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -3,22 +3,38 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logout } from '../store';
 
-const Navbar = () => {
+const Navbar = ({ handleClick }) => {
   return (
-    <header className="navbar">
-      <div className="navbar_wrapper">
-        <Link to="/">
+    <header className='navbar'>
+      <div className='navbar_wrapper'>
+        <Link to='/'>
           <img
-            className="logo"
-            src="/assets/logo.svg"
-            height="100px"
-            width="200px"
+            className='logo'
+            src='/assets/logo.svg'
+            height='100px'
+            width='200px'
           />
         </Link>
       </div>
-      <button className="logout">Logout</button>
+      <a href='#' onClick={handleClick}>
+        <button className='logout'>Logout</button>
+      </a>
     </header>
   );
 };
 
-export default Navbar;
+const mapState = (state) => {
+  return {
+    isLoggedIn: !!state.auth.id,
+  };
+};
+
+const mapDispatch = (dispatch) => {
+  return {
+    handleClick() {
+      dispatch(logout());
+    },
+  };
+};
+
+export default connect(mapState, mapDispatch)(Navbar);
