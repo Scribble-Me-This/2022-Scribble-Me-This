d6097447a4d54f2f3732d63e1dd0fd5141d2fab3
DavidMBurke <burkedm94@gmail.com>
2022-08-25 17:17:24 -0400

    implement loading trained model and testing with test data

--- client/App.js
+++ client/App.js
@@ -1,44 +1,62 @@
-import React from 'react';
 import Navbar from './components/Navbar';
 import Routes from './Routes';
+import React from 'react';
+import axios from 'axios';
 import ml5 from 'ml5';
-// import { Instance } from './components/instance';
-import model from './model.json';
-import metadata from './model_meta.json';
-import weights from './model.weights.bin';
-
 
 const options = {
   task: "classification",
   debug: false,
 };
-const nn = ml5.neuralNetwork(options);
 
-// const modelDetails = {
-//   model: './model.json',
-//   metadata: './model_meta.json',
-//   weights: './model.weights.bin'
-// }
-// console.log("!!!", modelDetails)
+let testingData = [];
+
+const nn = ml5.neuralNetwork(options);
+let testingdata;
+
+axios.get("/api/data").then((res) => {
+  testingData = res.data.trainingData;
+  let dataSlice = testingData.slice(10000,10010)
+  console.log(`adding ${dataSlice.length} drawings to testing data..`)
+  dataSlice.forEach((item) => {
+    let input = {};
+    let output = {};
+    for (let i = 0; i < 784; i++) {
+      input[i] = item.input[i];
+    }  
+    output.drawing = item.output;
+    nn.classify(input, handleResults)
+  });  
+})
+
+function handleResults(error, result) {
+  if(error) {
+    console.error(error);
+    return;
+  }
+  console.log(result);
+}
 
 const modelDetails = {
-  model: model,
-  metadata: metadata,
-  weights: weights
+  model: './model.json',
+  metadata: './model_meta.json',
+  weights: './model.weights.bin'
 }
-console.log("!!!", modelDetails)
-
-setTimeout(()=>{nn.load(modelDetails, console.log( "loaded Model"))},5000);
-console.log(nn);
 
 const App = () => {
+  console.log("!!!", modelDetails)
+  nn.load(modelDetails, finishLoad());
+  console.log(nn);
   return (
     <div>
       <Navbar />
-      {/* <Instance /> */}
       <Routes />
     </div>
   );
 };
+
+function finishLoad() {
+  console.log("finished loading")
+}
 
 export default App;
