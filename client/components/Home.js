40e7a62e817500981e1a0d213287561138293355
Harrison J.K <hjk72@cornell.edu>
2022-08-29 19:57:21 -0400

    Homepage

--- client/components/Home.js
+++ client/components/Home.js
@@ -9,6 +9,37 @@ export const Home = (props) => {
   const { username } = props;
   return (
     <div>
+      <div class="centerWrapper">
+        <input
+          class="smallMargin homeButtons"
+          type="text"
+          placeholder="Name Here!"
+        ></input>
+        <button class="join homeButtons hov">View Lobbies</button>
+        <div class="flex">
+          <button class="homeButtons hov">Create Room</button>
+          <div class="smallWrapper">
+            <img
+              className="flex"
+              src="/assets/leftArrow.svg"
+              height="50px"
+              width="100px"
+            />
+            <img
+              className="flex"
+              src="/assets/rightArrow.svg"
+              height="50px"
+              width="100px"
+            />
+          </div>
+          <input
+            class="homeButtons input40"
+            type="text"
+            placeholder="Code"
+          ></input>
+          <button class="homeButtons hov">Submit</button>
+        </div>
+      </div>
       <h3>Welcome, {username}</h3>
       <LobbyBrowser />
     </div>
